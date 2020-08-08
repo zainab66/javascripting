a9fa340e301a158b54bc3d846d775f387a54da56
zainab66 <zainab66r@yahoo.com>
2020-08-08 07:28:55 +0000

    Module2 javascripting

---
+++ javascripting/function-arguments.js
@@ -0,0 +1,6 @@
+function math (firstArg, secondArg,thirdArg) { 
+ let a = secondArg*thirdArg;
+let c = a +  firstArg;
+return c;
+     }  
+       console.log(math(53,61,67));
---
+++ javascripting/ifStatement.js
@@ -0,0 +1,8 @@
+ let fruit = "6"  
+
+    if (fruit > 5) {  
+       console.log('The fruit name has more than five characters.')  
+     } else {  
+       console.log('The fruit name has five characters or less.')  
+     }  
+
